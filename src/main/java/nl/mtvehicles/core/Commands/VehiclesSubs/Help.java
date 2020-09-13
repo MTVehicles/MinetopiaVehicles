e67d07d31d55c286da0181b9195e0716128d34db
GamerJoep_ <joep.rasters@gmail.com>
2020-09-13 16:36:39 +0200

    Pom fixed & Subcommand

--- src/main/java/nl/mtvehicles/core/Commands/VehiclesSubs/Help.java
+++ src/main/java/nl/mtvehicles/core/Commands/VehiclesSubs/Help.java
@@ -1,5 +1,6 @@
 package nl.mtvehicles.core.Commands.VehiclesSubs;
 
+import nl.mtvehicles.core.Infrastructure.Helpers.Text;
 import nl.mtvehicles.core.Infrastructure.Models.MTVehicleSubCommand;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -8,7 +9,7 @@ public class Help extends MTVehicleSubCommand {
     @Override
     public boolean execute(CommandSender sender, Command cmd, String s, String[] args) {
 
-        sender.sendMessage("Ado");
+        sender.sendMessage(Text.colorize("&1Super vet!!!"));
 
 
         return false;
--- src/main/java/nl/mtvehicles/core/Infrastructure/Models/MTVehicleCommand.java
+++ src/main/java/nl/mtvehicles/core/Infrastructure/Models/MTVehicleCommand.java
@@ -6,10 +6,17 @@ import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 
 public abstract class MTVehicleCommand implements CommandExecutor {
+    public CommandSender commandSender;
+
+    
+
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        this.commandSender = commandSender;
         return this.execute(commandSender, command, s, strings);
     }
+
+
 
     public abstract boolean execute(CommandSender sender, Command cmd, String s, String[] args);
 }
