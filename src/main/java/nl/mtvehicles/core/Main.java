729881b1e1bf58d623a7861387c3cc521b9e6b14
Jeffrey <j.hoonakker@gmail.com>
2020-09-13 14:54:35 +0200

    Initial commit

---
+++ src/main/java/nl/mtvehicles/core/Main.java
@@ -0,0 +1,15 @@
+package nl.mtvehicles.core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        //
+    }
+
+    @Override
+    public void onDisable() {
+        //
+    }
+}
