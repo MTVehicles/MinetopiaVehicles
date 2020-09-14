66c22a3e0e2616e0742825d79e9ab357a50d6541
Jeffrey <j.hoonakker@gmail.com>
2020-09-14 08:43:41 +0200

    Added sendMessage function to MessagesConfig

--- src/main/java/nl/mtvehicles/core/Infrastructure/Data/Config/MessagesConfig.java
+++ src/main/java/nl/mtvehicles/core/Infrastructure/Data/Config/MessagesConfig.java
@@ -2,6 +2,9 @@ package nl.mtvehicles.core.Infrastructure.Data.Config;
 
 import nl.mtvehicles.core.Infrastructure.Helpers.Text;
 import nl.mtvehicles.core.Infrastructure.Models.Config;
+import org.bukkit.command.CommandSender;
+
+import java.util.List;
 
 public class MessagesConfig extends Config {
     public MessagesConfig() {
@@ -10,5 +13,15 @@ public class MessagesConfig extends Config {
 
     public String getMessage(String key) {
         return Text.colorize((String) this.getConfig().get(key));
+    }
+
+    public void sendMessage(CommandSender sender, String key) {
+        Object object = this.getConfig().get(key);
+        if (object instanceof List) {
+            for (String s : this.getConfig().getStringList(key)) {
+                sender.sendMessage(Text.colorize(s));
+            }
+        }
+        sender.sendMessage(Text.colorize(String.valueOf(object)));
     }
 }
