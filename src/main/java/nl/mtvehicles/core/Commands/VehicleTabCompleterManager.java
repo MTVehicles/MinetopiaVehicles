5f7fe20e70d4e5b8b91d4d678474cf6e09e1192a
Jazzkuh <32953684+Jazzkuh@users.noreply.github.com>
2021-06-29 23:32:33 +0200

    Fix shitty tab completer

--- src/main/java/nl/mtvehicles/core/Commands/VehicleTabCompleterManager.java
+++ src/main/java/nl/mtvehicles/core/Commands/VehicleTabCompleterManager.java
@@ -1,20 +1,26 @@
 package nl.mtvehicles.core.Commands;
 
 import nl.mtvehicles.core.Main;
+import org.bukkit.util.StringUtil;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class VehicleTabCompleterManager implements org.bukkit.command.TabCompleter {
     @Override
     public @Nullable List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
         if (strings.length == 1) {
-            return new ArrayList<>(Main.subcommands.keySet());
+            return getApplicableTabCompleters(args[0], Main.subcommands.keySet());
         }
         return null;
+    }
+
+    private List<String> getApplicableTabCompleters(String arg, Collection<String> completions) {
+        return StringUtil.copyPartialMatches(arg, completions, new ArrayList<>(completions.size()));
     }
 }
