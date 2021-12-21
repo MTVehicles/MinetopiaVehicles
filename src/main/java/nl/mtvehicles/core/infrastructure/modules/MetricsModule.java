0cd21a69d5a80170cdb9971a410e9e9fe8410fd1
GamerJoep_ <joep.rasters@gmail.com>
2021-12-21 18:11:17 +0100

    Added Metrics Module

---
+++ src/main/java/nl/mtvehicles/core/infrastructure/modules/MetricsModule.java
@@ -0,0 +1,19 @@
+package nl.mtvehicles.core.infrastructure.modules;
+
+import lombok.Getter;
+import lombok.Setter;
+import nl.mtvehicles.core.Main;
+import nl.mtvehicles.core.infrastructure.dataconfig.Metrics;
+
+public class MetricsModule {
+    private static @Getter
+    @Setter
+    MetricsModule instance;
+
+    public MetricsModule() {
+        Metrics metrics = new Metrics(Main.instance, 5932);
+        metrics.addCustomChart(new Metrics.SimplePie("used_language", () -> {
+            return Main.defaultConfig.getConfig().getString("messagesLanguage");
+        }));
+    }
+}
