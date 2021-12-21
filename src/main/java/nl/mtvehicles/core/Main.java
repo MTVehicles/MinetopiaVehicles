b279a6d49cbadda01aa6156298a5a85ee068a370
GamerJoep_ <joep.rasters@gmail.com>
2021-12-21 18:04:28 +0100

    Code clearer with explanation in the Main class

--- src/main/java/nl/mtvehicles/core/Main.java
+++ src/main/java/nl/mtvehicles/core/Main.java
@@ -6,6 +6,7 @@ import nl.mtvehicles.core.events.*;
 import nl.mtvehicles.core.infrastructure.dataconfig.*;
 import nl.mtvehicles.core.infrastructure.helpers.TextUtils;
 import nl.mtvehicles.core.infrastructure.models.ConfigUtils;
+import nl.mtvehicles.core.infrastructure.models.ListenersModule;
 import nl.mtvehicles.core.infrastructure.models.MTVehicleSubCommand;
 import nl.mtvehicles.core.inventory.InventoryClickEvent;
 import nl.mtvehicles.core.inventory.InventoryCloseEvent;
@@ -13,6 +14,7 @@ import nl.mtvehicles.core.movement.MovementManager;
 import org.bukkit.Bukkit;
 import org.bukkit.command.PluginCommand;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -38,6 +40,7 @@ public class Main extends JavaPlugin {
 
         instance = this;
 
+        //Check if the plugin is using the correct version
         if (!version.equals("v1_12_R1") && !version.equals("v1_13_R2") && !version.equals("v1_15_R1") && !version.equals("v1_16_R3") && !version.contains("v1_17") && !version.contains("v1_18")) {
             getLogger().info("-------------------------------------------------------");
             getLogger().info("Your Server version is not supported by the plugin.");
@@ -50,47 +53,61 @@ public class Main extends JavaPlugin {
         PluginDescriptionFile pdf = this.getDescription();
         String versions = pdf.getVersion();
 
+        //Plugin enable message for the console
         getLogger().info("Plugin has been loaded!");
         getLogger().info("--------------------------");
         getLogger().info("Welcome by MTVehicles " + versions + " !");
         getLogger().info("Thanks for using our plugin.");
         getLogger().info("--------------------------");
 
+
+        //Setting the command for the plugin
         PluginCommand pluginCommand = Main.instance.getCommand("minetopiavehicles");
 
         if (pluginCommand != null) {
+            //Register the /minetopiavehicles command
             pluginCommand.setExecutor(new VehicleSubCommandManager());
+
+            //Register tabcompleter for the command /minetopiavehicles
             pluginCommand.setTabCompleter(new VehicleTabCompleterManager());
         }
 
-        Bukkit.getPluginManager().registerEvents(new InventoryClickEvent(), this);
-        Bukkit.getPluginManager().registerEvents(new VehiclePlaceEvent(), this);
-        Bukkit.getPluginManager().registerEvents(new VehicleClickEvent(), this);
-        Bukkit.getPluginManager().registerEvents(new VehicleLeaveEvent(), this);
-        Bukkit.getPluginManager().registerEvents(new ChatEvent(), this);
-        Bukkit.getPluginManager().registerEvents(new VehicleEntityEvent(), this);
-        Bukkit.getPluginManager().registerEvents(new JoinEvent(), this);
-        Bukkit.getPluginManager().registerEvents(new LeaveEvent(), this);
-        Bukkit.getPluginManager().registerEvents(new VehicleVoucherEvent(), this);
-        Bukkit.getPluginManager().registerEvents(new InventoryCloseEvent(), this);
+        //Register the Listeners
+        new ListenersModule();
 
+        //bStats mertrics https://bstats.org/plugin/bukkit/MTVehicles
         Metrics metrics = new Metrics(this, 5932);
         metrics.addCustomChart(new Metrics.SimplePie("used_language", () -> {
             return defaultConfig.getConfig().getString("messagesLanguage");
         }));
 
+        //Looping the online people in the server
         for (Player p : Bukkit.getServer().getOnlinePlayers()) {
+
+            //Checking if someone is in a vehicle while starting
             if (p.isInsideVehicle()) {
+
+                //kick the player if he is in a vehicle when starting the plugin
                 p.kickPlayer(TextUtils.colorize(Main.messagesConfig.getMessage("reloadInVehicle")));
             }
+
+            //Select correct packing for player inputs
             MovementManager.MovementSelector(p);
         }
 
+        //Getting the default config
         File defaultconfig = new File(getDataFolder(), "config.yml");
+
+        //Getting the vehicles.yml
         File vehicleconfig = new File(getDataFolder(), "vehicles.yml");
+
+        //Make a format to rename old configs
         SimpleDateFormat formatter = new SimpleDateFormat("dd_MM_yyyy-HH_mm_ss");
         Date date = new Date();
+
+        //Compare the config version with the plugin version
         if (!getConfig().get("Config-Versie").equals(versions)) {
+            //Renaming the config files
             defaultconfig.renameTo(new File(getDataFolder(), "configOld_" + formatter.format(date) + ".yml"));
             vehicleconfig.renameTo(new File(getDataFolder(), "vehiclesOld_" + formatter.format(date) + ".yml"));
             saveDefaultConfig();
@@ -101,6 +118,10 @@ public class Main extends JavaPlugin {
         configList.add(vehiclesConfig);
         configList.add(defaultConfig);
         configList.forEach(ConfigUtils::reload);
+    }
+
+    public void registerListener(Listener listener) {
+        Bukkit.getPluginManager().registerEvents(listener, this);
     }
 
     public static String fol() {
---
+++ src/main/java/nl/mtvehicles/core/infrastructure/models/ListenersModule.java
@@ -0,0 +1,27 @@
+package nl.mtvehicles.core.infrastructure.models;
+
+import lombok.Getter;
+import lombok.Setter;
+import nl.mtvehicles.core.Main;
+import nl.mtvehicles.core.events.*;
+import nl.mtvehicles.core.inventory.InventoryClickEvent;
+import nl.mtvehicles.core.inventory.InventoryCloseEvent;
+
+public class ListenersModule {
+    private static @Getter
+    @Setter
+    ListenersModule instance;
+
+    public ListenersModule() {
+        Main.instance.registerListener(new InventoryClickEvent());
+        Main.instance.registerListener(new VehiclePlaceEvent());
+        Main.instance.registerListener(new VehicleClickEvent());
+        Main.instance.registerListener(new VehicleLeaveEvent());
+        Main.instance.registerListener(new ChatEvent());
+        Main.instance.registerListener(new VehicleEntityEvent());
+        Main.instance.registerListener(new JoinEvent());
+        Main.instance.registerListener(new LeaveEvent());
+        Main.instance.registerListener(new VehicleVoucherEvent());
+        Main.instance.registerListener(new InventoryCloseEvent());
+    }
+}
