2491954a7508ba65faff27df4bfe89b2e3392c7b
Nikd0 <47473562+Zettovec@users.noreply.github.com>
2022-01-01 19:12:19 +0100

    Add Vault softdepend
    For working with economy

---
+++ src/main/java/nl/mtvehicles/core/infrastructure/dependencies/VaultUtils.java
@@ -0,0 +1,41 @@
+package nl.mtvehicles.core.infrastructure.dependencies;
+
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import nl.mtvehicles.core.Main;
+
+public class VaultUtils {
+    //This is only called if DependencyModule made sure that Vault is installed.
+
+    private static Economy economy = null;
+
+    public VaultUtils(){
+        setupEconomy();
+    }
+
+    private static void setupEconomy(){
+        RegisteredServiceProvider<Economy> rsp = Main.instance.getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) return;
+        economy = rsp.getProvider();
+    }
+
+    public boolean depositMoneyPlayer(OfflinePlayer p, double price){ //true - successful, false - error
+        if (!isPriceOk(price)) return false;
+        if (!economy.hasAccount(p)) return false;
+
+        return economy.depositPlayer(p, price).transactionSuccess();
+    }
+
+    public boolean withdrawMoneyPlayer(OfflinePlayer p, double price){ //true - payed, false - didn't
+        if (!isPriceOk(price)) return false;
+        if (!economy.hasAccount(p)) return false;
+        if (!economy.has(p, price)) return false;
+
+        return economy.withdrawPlayer(p, price).transactionSuccess();
+    }
+
+    private boolean isPriceOk(double price){ //returns true if ok
+        return (price > 0);
+    }
+}
