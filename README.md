3d3095de035d55fff6594f224123c046b3a2f5f0
Nikd0 <47473562+Zettovec@users.noreply.github.com>
2022-03-27 17:18:26 +0200

    More placeholders?

--- README.md
+++ README.md
@@ -162,9 +162,19 @@ You can edit the behaviour **in the config** where you may also find information
 
 MTVehicles offers special placeholders for you to use.
 
+#### Global placeholders
+
+| Placeholder                | Description                                            |
+|----------------------------|--------------------------------------------------------|
+| `mtv_fuel_pricePerLitre`   | Price of fuel (used while filling jerrycans)           |
+
+#### Per-player placeholders
+
 | Placeholder                | Description                                            |
 |----------------------------|--------------------------------------------------------|
 | `mtv_vehicle_licensePlate` | License plate of a vehicle a player **is sitting in**  |
+| `mtv_vehicle_name`         | Name of a vehicle a player is sitting in               |
+| `mtv_vehicle_type`         | Type of a vehicle a player is sitting in               |
 | `mtv_vehicle_fuel`         | Remaining fuel of a vehicle a player is sitting in     |
 
 > More placeholders are expected to be added in the future...
--- src/main/java/nl/mtvehicles/core/infrastructure/dependencies/PlaceholderUtils.java
+++ src/main/java/nl/mtvehicles/core/infrastructure/dependencies/PlaceholderUtils.java
@@ -4,7 +4,9 @@ import me.clip.placeholderapi.PlaceholderAPI;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import nl.mtvehicles.core.Main;
 import nl.mtvehicles.core.infrastructure.annotations.ToDo;
+import nl.mtvehicles.core.infrastructure.dataconfig.DefaultConfig;
 import nl.mtvehicles.core.infrastructure.dataconfig.VehicleDataConfig;
+import nl.mtvehicles.core.infrastructure.enums.VehicleType;
 import nl.mtvehicles.core.infrastructure.helpers.VehicleData;
 import nl.mtvehicles.core.infrastructure.models.VehicleUtils;
 import nl.mtvehicles.core.infrastructure.modules.ConfigModule;
@@ -48,6 +50,20 @@ public class PlaceholderUtils extends PlaceholderExpansion {
             return VehicleUtils.getLicensePlate(p.getPlayer().getVehicle());
         }
 
+        if (parameter.equalsIgnoreCase("vehicle_name")){
+            if (!p.isOnline()) return "";
+            if (!isInsideVehicle(p)) return "";
+            String licensePlate =  VehicleUtils.getLicensePlate(p.getPlayer().getVehicle());
+            return ConfigModule.vehicleDataConfig.get(licensePlate, VehicleDataConfig.Option.NAME).toString();
+        }
+
+        if (parameter.equalsIgnoreCase("vehicle_type")){
+            if (!p.isOnline()) return "";
+            if (!isInsideVehicle(p)) return "";
+            String licensePlate =  VehicleUtils.getLicensePlate(p.getPlayer().getVehicle());
+            return VehicleType.valueOf(ConfigModule.vehicleDataConfig.get(licensePlate, VehicleDataConfig.Option.VEHICLE_TYPE).toString()).getName();
+        }
+
         if (parameter.equalsIgnoreCase("vehicle_fuel")){
             if (!p.isOnline()) return "";
             if (!isInsideVehicle(p)) return "";
@@ -55,6 +71,10 @@ public class PlaceholderUtils extends PlaceholderExpansion {
             if (!(boolean) ConfigModule.vehicleDataConfig.get(licensePlate, VehicleDataConfig.Option.FUEL_ENABLED)) return "";
             DecimalFormat df = new DecimalFormat("#.##");
             return df.format(VehicleData.fuel.get(licensePlate)) + "%";
+        }
+
+        if (parameter.equalsIgnoreCase("fuel_pricePerLitre")){
+            return ConfigModule.defaultConfig.get(DefaultConfig.Option.GAS_STATIONS_FILL_JERRYCANS_PRICE_PER_LITRE).toString();
         }
 
         return null;
