24322fde679284e0335c371c64ddbbe2cd8cad21
Nikd0 <47473562+Zettovec@users.noreply.github.com>
2022-04-10 17:08:09 +0200

    Add #spawnVehicle(...) method

--- src/main/java/nl/mtvehicles/core/listeners/VehiclePlaceListener.java
+++ src/main/java/nl/mtvehicles/core/listeners/VehiclePlaceListener.java
@@ -47,6 +47,8 @@ public class VehiclePlaceListener implements Listener {
                 || e.getClickedBlock() == null
         ) return;
 
+        if (!action.equals(Action.RIGHT_CLICK_BLOCK)) return;
+
         if (e.getHand() != EquipmentSlot.HAND) {
             e.setCancelled(true);
             e.getPlayer().sendMessage(TextUtils.colorize(ConfigModule.messagesConfig.getMessage(Message.WRONG_HAND)));
@@ -54,15 +56,11 @@ public class VehiclePlaceListener implements Listener {
         }
 
         String license = VehicleUtils.getLicensePlate(item);
-        if (license == null) {
-            return;
-        }
+        if (license == null) return;
+
         if (!VehicleUtils.existsByLicensePlate(license)) {
             ConfigModule.messagesConfig.sendMessage(p, Message.VEHICLE_NOT_FOUND);
             e.setCancelled(true);
-            return;
-        }
-        if (!action.equals(Action.RIGHT_CLICK_BLOCK)) {
             return;
         }
 
@@ -73,9 +71,8 @@ public class VehiclePlaceListener implements Listener {
         // You can set more things take a look at VehiclePlaceEvent
         Bukkit.getPluginManager().callEvent(vehiclePlaceEvent);
 
-        if (vehiclePlaceEvent.isCancelled()) {
-            return;
-        }
+        if (vehiclePlaceEvent.isCancelled()) return;
+
 
         Location loc = e.getClickedBlock().getLocation();
         e.setCancelled(true);
@@ -96,51 +93,9 @@ public class VehiclePlaceListener implements Listener {
         }
 
         Location location = new Location(loc.getWorld(), loc.getX(), loc.getY() + 1, loc.getZ());
-        ArmorStand as = location.getWorld().spawn(location, ArmorStand.class);
-        as.setVisible(false);
-        as.setCustomName("MTVEHICLES_SKIN_" + license);
-        as.getEquipment().setHelmet(item);
-        ArmorStand as2 = location.getWorld().spawn(location, ArmorStand.class);
-        as2.setVisible(false);
-        as2.setCustomName("MTVEHICLES_MAIN_" + license);
-        Vehicle vehicle = VehicleUtils.getByLicensePlate(license);
-        List<Map<String, Double>> seats = (List<Map<String, Double>>) vehicle.getVehicleData().get("seats");
+
+        VehicleUtils.spawnVehicle(license, location);
         p.getInventory().remove(p.getEquipment().getItemInHand());
         p.sendMessage(TextUtils.colorize(ConfigModule.messagesConfig.getMessage(Message.VEHICLE_PLACE).replace("%p%", VehicleUtils.getByLicensePlate(license).getOwnerName())));
-        for (int i = 1; i <= seats.size(); i++) {
-            Map<String, Double> seat = seats.get(i - 1);
-            if (i == 1) {
-                Location location2 = new Location(location.getWorld(), location.getX() + seat.get("x"), location.getY() + seat.get("y"), location.getZ() + seat.get("z"));
-                ArmorStand as3 = location2.getWorld().spawn(location2, ArmorStand.class);
-                as3.setCustomName("MTVEHICLES_MAINSEAT_" + license);
-                as3.setGravity(false);
-                as3.setVisible(false);
-            }
-        }
-        List<Map<String, Double>> wiekens = (List<Map<String, Double>>) vehicle.getVehicleData().get("wiekens");
-        if (ConfigModule.vehicleDataConfig.getType(license).isHelicopter()) {
-            for (int i = 1; i <= wiekens.size(); i++) {
-                Map<?, ?> seat = wiekens.get(i - 1);
-                if (i == 1) {
-                    Location location2 = new Location(location.getWorld(), location.getX() + (Double) seat.get("z"), (Double) location.getY() + (Double) seat.get("y"), location.getZ() + (Double) seat.get("x"));
-                    ArmorStand as3 = location2.getWorld().spawn(location2, ArmorStand.class);
-                    as3.setCustomName("MTVEHICLES_WIEKENS_" + license);
-                    as3.setGravity(false);
-                    as3.setVisible(false);
-                    if ((boolean) ConfigModule.defaultConfig.get(DefaultConfig.Option.HELICOPTER_BLADES_ALWAYS_ON)) {
-                        ItemStack car = (new ItemFactory(Material.getMaterial("DIAMOND_HOE"))).setDurability((short) 1058).setName(TextUtils.colorize("&6Wieken")).setNBT("mtvehicles.kenteken", license).toItemStack();
-                        ItemMeta im = car.getItemMeta();
-                        List<String> itemlore = new ArrayList<>();
-                        itemlore.add(TextUtils.colorize("&a"));
-                        itemlore.add(TextUtils.colorize("&a" + license));
-                        itemlore.add(TextUtils.colorize("&a"));
-                        im.setLore(itemlore);
-                        im.setUnbreakable(true);
-                        car.setItemMeta(im);
-                        as3.setHelmet((ItemStack) seat.get("item"));
-                    }
-                }
-            }
-        }
     }
 }
