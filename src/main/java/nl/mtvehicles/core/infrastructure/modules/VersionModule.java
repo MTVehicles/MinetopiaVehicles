1e599df9142b9765b22008d3584e730b1a7c3961
Nikd0 <47473562+Zettovec@users.noreply.github.com>
2022-05-01 22:06:33 +0200

    Bug fixes

--- src/main/java/nl/mtvehicles/core/infrastructure/modules/VersionModule.java
+++ src/main/java/nl/mtvehicles/core/infrastructure/modules/VersionModule.java
@@ -3,8 +3,7 @@ package nl.mtvehicles.core.infrastructure.modules;
 import lombok.Getter;
 import lombok.Setter;
 import nl.mtvehicles.core.Main;
-import nl.mtvehicles.core.infrastructure.enums.Version;
-import nl.mtvehicles.core.movement.PacketHandler;
+import nl.mtvehicles.core.infrastructure.enums.ServerVersion;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginDescriptionFile;
 
@@ -32,26 +31,29 @@ public class VersionModule {
         serverSoftware = Bukkit.getName();
     }
 
-    public static Version getServerVersion(){
-        Version returns = null;
+    public static ServerVersion getServerVersion(){
+        ServerVersion returns = null;
         switch (serverVersion) {
             case "v1_12_R1":
-                returns = Version.v1_12;
+                returns = ServerVersion.v1_12;
                 break;
             case "v1_13_R2":
-                returns = Version.v1_13;
+                returns = ServerVersion.v1_13;
                 break;
             case "v1_15_R1":
-                returns = Version.v1_15;
+                returns = ServerVersion.v1_15;
                 break;
             case "v1_16_R3":
-                returns = Version.v1_16;
+                returns = ServerVersion.v1_16;
                 break;
             case "v1_17_R1":
-                returns = Version.v1_17;
+                returns = ServerVersion.v1_17;
+                break;
+            case "v1_18_R1":
+                returns = ServerVersion.v1_18_R1;
                 break;
             case "v1_18_R2":
-                returns = Version.v1_18;
+                returns = ServerVersion.v1_18_R2;
                 break;
         }
         return returns;
@@ -69,15 +71,9 @@ public class VersionModule {
 
         else if (!Bukkit.getVersion().contains("1.12.2") && !Bukkit.getVersion().contains("1.13.2") && !Bukkit.getVersion().contains("1.15.2") && !Bukkit.getVersion().contains("1.16.5") && !Bukkit.getVersion().contains("1.17.1") && !Bukkit.getVersion().contains("1.18.2")) {
             logger.warning("--------------------------");
-            logger.warning("Your Server does not run the latest patch version (e.g. you may be running 1.16.3 instead of 1.16.5 etc...).");
-            logger.warning("The plugin WILL load but you are NOT eligible for any support unless you update the server.");
-            logger.warning("--------------------------");
-        }
-
-        else if (serverSoftware.equals("Purpur")){
-            logger.warning("--------------------------");
-            logger.warning("Your Server is running Purpur (fork of Paper).");
-            logger.warning("The plugin WILL load but it MAY NOT work properly. Bear in mind that support for Purpur is NOT guaranteed.");
+            logger.warning("Your Server does not run the latest patch version (e.g. you may be running 1.18 instead of 1.18.2 etc...).");
+            logger.warning("The plugin WILL load but it MAY NOT work properly. UPDATE.");
+            logger.warning("Check the supported versions here: https://wiki.mtvehicles.eu/faq.html");
             logger.warning("--------------------------");
         }
 
