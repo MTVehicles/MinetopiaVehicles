f0add279e26b10a6182fdcb261e154578ea3ced2
Nikd0 <47473562+Zettovec@users.noreply.github.com>
2022-05-01 22:11:03 +0200

    Support for 1.18.1 and 1.18.2 (v2.4.0-dev1)

--- src/main/java/nl/mtvehicles/core/infrastructure/enums/ServerVersion.java
+++ src/main/java/nl/mtvehicles/core/infrastructure/enums/ServerVersion.java
@@ -6,30 +6,35 @@ public enum ServerVersion {
     v1_15,
     v1_16,
     v1_17,
-    v1_18;
+    v1_18_R1,
+    v1_18_R2;
 
     public boolean is1_12(){
-        return this.equals(ServerVersion.v1_12);
+        return this.equals(v1_12);
     }
 
     public boolean is1_13(){
-        return this.equals(ServerVersion.v1_13);
+        return this.equals(v1_13);
     }
 
     public boolean is1_15(){
-        return this.equals(ServerVersion.v1_15);
+        return this.equals(v1_15);
     }
 
     public boolean is1_16(){
-        return this.equals(ServerVersion.v1_16);
+        return this.equals(v1_16);
     }
 
     public boolean is1_17(){
-        return this.equals(ServerVersion.v1_17);
+        return this.equals(v1_17);
     }
 
-    public boolean is1_18(){
-        return this.equals(ServerVersion.v1_18);
+    public boolean is1_18_R1(){
+        return this.equals(v1_18_R1);
+    }
+
+    public boolean is1_18_R2(){
+        return this.equals(v1_18_R2);
     }
 
 }
