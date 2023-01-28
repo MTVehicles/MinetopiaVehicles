bec21d76691a8ae3229c192ffd0c742eadaaa780
Manuel Palumbo <manuel.palumbo@email.com>
2023-01-28 17:07:51 +0100

    Fix: Extended armor-stand ticking compatibility up to 1.12.2

--- src/main/java/nl/mtvehicles/core/infrastructure/models/VehicleUtils.java
+++ src/main/java/nl/mtvehicles/core/infrastructure/models/VehicleUtils.java
@@ -52,6 +52,7 @@ public final class VehicleUtils {
         if (!existsByLicensePlate(licensePlate)) throw new IllegalArgumentException("Vehicle does not exists.");
 
         ArmorStand standSkin = location.getWorld().spawn(location, ArmorStand.class);
+        allowTicking(standSkin);
         standSkin.setVisible(false);
         standSkin.setCustomName("MTVEHICLES_SKIN_" + licensePlate);
         standSkin.getEquipment().setHelmet(
@@ -95,6 +96,8 @@ public final class VehicleUtils {
                 itemMeta.setLore(lore);
                 itemMeta.setUnbreakable(true);
                 rotor.setItemMeta(itemMeta);
+
+                allowTicking(standRotors);
                 standRotors.setHelmet((ItemStack) blade.get("item"));
             }
         }
@@ -601,7 +604,8 @@ public final class VehicleUtils {
                             Location location2 = new Location(location.getWorld(), location.getX() + Double.valueOf(seat.get("z")), location.getY() + Double.valueOf(seat.get("y")), location.getZ() + Double.valueOf(seat.get("x")));
 
                             ArmorStand as = location2.getWorld().spawn(location2, ArmorStand.class);
-                            applyDefaultTraits(as);
+                            allowTicking(as);
+                            as.setVisible(false);
                             as.setCustomName("MTVEHICLES_SEAT" + i + "_" + licensePlate);
                             as.setGravity(false);
 
@@ -621,7 +625,8 @@ public final class VehicleUtils {
                                 VehicleData.wiekenz.put("MTVEHICLES_WIEKENS_" + licensePlate, (Double) seat.get("z"));
 
                                 ArmorStand as = location2.getWorld().spawn(location2, ArmorStand.class);
-                                applyDefaultTraits(as);
+                                allowTicking(as);
+                                as.setVisible(false);
                                 as.setCustomName("MTVEHICLES_WIEKENS_" + licensePlate);
                                 as.setGravity(false);
                                 as.setHelmet((ItemStack) seat.get("item"));
@@ -641,7 +646,8 @@ public final class VehicleUtils {
      */
     private static void basicStandCreator(String license, String type, Location location, ItemStack item, Boolean gravity) {
         ArmorStand as = location.getWorld().spawn(location, ArmorStand.class);
-        applyDefaultTraits(as);
+        allowTicking(as);
+        as.setVisible(false);
         as.setCustomName("MTVEHICLES_" + type + "_" + license);
         as.setHelmet(item);
         as.setGravity(gravity);
@@ -649,12 +655,10 @@ public final class VehicleUtils {
         VehicleData.autostand.put("MTVEHICLES_" + type + "_" + license, as);
     }
 
-    private static void applyDefaultTraits(ArmorStand armorStand) {
+    private static void allowTicking(ArmorStand armorStand) {
         if(PaperUtils.isRunningPaper) {
             armorStand.setCanTick(true);
         }
-
-        armorStand.setVisible(false);
     }
 
     /**
@@ -663,7 +667,8 @@ public final class VehicleUtils {
     private static void mainSeatStandCreator(String license, Location location, Player p, double x, double y, double z) {
         Location location2 = new Location(location.getWorld(), location.getX() + Double.valueOf(z), location.getY() + Double.valueOf(y), location.getZ() + Double.valueOf(z));
         ArmorStand as = location2.getWorld().spawn(location2, ArmorStand.class);
-        applyDefaultTraits(as);
+        allowTicking(as);
+        as.setVisible(false);
         as.setCustomName("MTVEHICLES_MAINSEAT_" + license);
         as.setGravity(false);
 
